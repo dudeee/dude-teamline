34f976ca9018c08e5acc7590bbc6800d54c9920f
Mahdi Dibaiee <mdibaiee@aol.com>
2016-01-30 18:08:11 +0330

    fix(logs): remove unwanted console.logs

--- src/commands/index.js
+++ src/commands/index.js
@@ -6,12 +6,9 @@ export default function (bot, uri) {
   const commandsPath = path.join(__dirname, commandsDir);
 
   const commands = fs.readdirSync(commandsPath);
-  console.log(commands);
-  console.log('---------------');
   commands
     .filter(name => name !== 'index.js')
     .forEach(file => {
-      console.log(`SHIT! => ${file}`);
       require(`${commandsPath}/${file}`)(bot, uri);
     });
 }
