fa448a13f1421768127cd92bcee7dd0afdf47848
Mohamad Jahani <m4m4lj@gmail.com>
2016-01-31 12:14:14 +0330

    feat(recess): implement recess request and recess list

--- src/commands/workhours.js
+++ src/commands/workhours.js
@@ -4,9 +4,11 @@ import { capitalize } from 'lodash';
 
 const printHours = hours => {
   if (!hours.length) {
-    return 'Nothing to show 😶';
+    return 'Oops! I get nothing to show! 😶';
   }
-  let output = ':timer_clock: Your working hours plan is:\n';
+  let output = (hours.length > 1)
+  ? ':timer_clock: Your working hours plan is:\n'
+  : `:timer_clock: Your working hours plan for *${hours[0].weekday}* is:\n`;
   output += hours.map((hour, id) =>
     `#${id + 1}- *${capitalize(hour.weekday)}* from *${hour.start}* to *${hour.end}*`).join('\n'
   );
@@ -14,20 +16,26 @@ const printHours = hours => {
 };
 
 export default async (bot, uri) => {
-  bot.listen(/(?:workhours|wh)\s?(\w+)?/i, async message => {
-    const [method] = message.match;
+  bot.listen(/(?:workhours?|wh)\s?(\w+)?/i, async message => {
+    const [time] = message.match;
     const employee = await findEmployee(uri, bot, message);
     let workHours;
-    if (!method || method === 'list') {
-      workHours = await request('get', `${uri}/employee/${employee.id}/workhours`);
-      message.reply(printHours(workHours));
-    }
-    if (method === 'tomorrow') {
+    if (time === 'tomorrow') {
       const today = new Date();
       const weekday = getWeekday(today.getDay() + 1);
       workHours = await request('get', `${uri}/employee/${employee.id}` +
       `/workhours?weekday=${weekday}`);
-      message.reply(printHours(workHours));
+      return message.reply(printHours(workHours));
     }
+
+    const weekdays = ['sun', 'mon', 'tue', 'wed', 'thu', 'fri', 'sat'];
+    if (weekdays.includes(time)) {
+      workHours = await request('get', `${uri}/employee/${employee.id}` +
+      `/workhours?weekday=${time}`);
+      return message.reply(printHours(workHours));
+    }
+
+    workHours = await request('get', `${uri}/employee/${employee.id}/workhours`);
+    message.reply(printHours(workHours));
   });
 };
