efb64d0e07ad300b3d3724fd66c20f58ffbf1fbc
Mohamad Jahani <m4m4lj@gmail.com>
2016-01-31 12:14:14 +0330

    feat(recess): implement recess request and recess list

--- build/commands/workhours.js
+++ build/commands/workhours.js
@@ -14,9 +14,9 @@ var _lodash = require('lodash');
 
 var printHours = function printHours(hours) {
   if (!hours.length) {
-    return 'Nothing to show 😶';
+    return 'Oops! I get nothing to show! 😶';
   }
-  var output = ':timer_clock: Your working hours plan is:\n';
+  var output = hours.length > 1 ? ':timer_clock: Your working hours plan is:\n' : ':timer_clock: Your working hours plan for *' + hours[0].weekday + '* is:\n';
   output += hours.map(function (hour, id) {
     return '#' + (id + 1) + '- *' + (0, _lodash.capitalize)(hour.weekday) + '* from *' + hour.start + '* to *' + hour.end + '*';
   }).join('\n');
@@ -29,14 +29,14 @@ exports['default'] = function callee$0$0(bot, uri) {
 
     while (1) switch (context$1$0.prev = context$1$0.next) {
       case 0:
-        bot.listen(/(?:workhours|wh)\s?(\w+)?/i, function callee$1$0(message) {
-          var _message$match, method, employee, workHours, today, weekday;
+        bot.listen(/(?:workhours?|wh)\s?(\w+)?/i, function callee$1$0(message) {
+          var _message$match, time, employee, workHours, today, weekday, weekdays;
 
           return regeneratorRuntime.async(function callee$1$0$(context$2$0) {
             while (1) switch (context$2$0.prev = context$2$0.next) {
               case 0:
                 _message$match = _slicedToArray(message.match, 1);
-                method = _message$match[0];
+                time = _message$match[0];
                 context$2$0.next = 4;
                 return regeneratorRuntime.awrap((0, _utils.findEmployee)(uri, bot, message));
 
@@ -44,36 +44,45 @@ exports['default'] = function callee$0$0(bot, uri) {
                 employee = context$2$0.sent;
                 workHours = undefined;
 
-                if (!(!method || method === 'list')) {
-                  context$2$0.next = 11;
-                  break;
-                }
-
-                context$2$0.next = 9;
-                return regeneratorRuntime.awrap((0, _utils.request)('get', uri + '/employee/' + employee.id + '/workhours'));
-
-              case 9:
-                workHours = context$2$0.sent;
-
-                message.reply(printHours(workHours));
-
-              case 11:
-                if (!(method === 'tomorrow')) {
-                  context$2$0.next = 18;
+                if (!(time === 'tomorrow')) {
+                  context$2$0.next = 13;
                   break;
                 }
 
                 today = new Date();
                 weekday = (0, _utils.getWeekday)(today.getDay() + 1);
-                context$2$0.next = 16;
+                context$2$0.next = 11;
                 return regeneratorRuntime.awrap((0, _utils.request)('get', uri + '/employee/' + employee.id + ('/workhours?weekday=' + weekday)));
 
-              case 16:
+              case 11:
+                workHours = context$2$0.sent;
+                return context$2$0.abrupt('return', message.reply(printHours(workHours)));
+
+              case 13:
+                weekdays = ['sun', 'mon', 'tue', 'wed', 'thu', 'fri', 'sat'];
+
+                if (!weekdays.includes(time)) {
+                  context$2$0.next = 19;
+                  break;
+                }
+
+                context$2$0.next = 17;
+                return regeneratorRuntime.awrap((0, _utils.request)('get', uri + '/employee/' + employee.id + ('/workhours?weekday=' + time)));
+
+              case 17:
+                workHours = context$2$0.sent;
+                return context$2$0.abrupt('return', message.reply(printHours(workHours)));
+
+              case 19:
+                context$2$0.next = 21;
+                return regeneratorRuntime.awrap((0, _utils.request)('get', uri + '/employee/' + employee.id + '/workhours'));
+
+              case 21:
                 workHours = context$2$0.sent;
 
                 message.reply(printHours(workHours));
 
-              case 18:
+              case 23:
               case 'end':
                 return context$2$0.stop();
             }
