49f005d881808d7871001b84a074982804ee8f64
Mohamad Jahani <m4m4lj@gmail.com>
2016-02-01 13:12:06 +0330

    feat(workhours): add human date parse to workhours date parser

--- src/commands/workhours.js
+++ src/commands/workhours.js
@@ -1,5 +1,6 @@
 import { request, findEmployee, getWeekday, clockEmoji } from '../utils';
 import { capitalize } from 'lodash';
+import humanDate from 'date.js';
 
 
 const printHours = hours => {
@@ -16,23 +17,28 @@ const printHours = hours => {
 };
 
 export default async (bot, uri) => {
-  bot.listen(/(?:workhours?|wh)\s?(\w+)?/i, async message => {
+  bot.listen(/(?:workhours?|wh)\s?(.+)?/i, async message => {
     const [time] = message.match;
     const employee = await findEmployee(uri, bot, message);
-    let workHours;
-    if (time === 'tomorrow') {
-      const today = new Date();
-      const weekday = getWeekday(today.getDay() + 1);
-      workHours = await request('get', `${uri}/employee/${employee.id}` +
-      `/workhours?weekday=${weekday}`);
-      return message.reply(printHours(workHours));
-    }
 
+    let workHours;
     const weekdays = ['sun', 'mon', 'tue', 'wed', 'thu', 'fri', 'sat'];
     if (weekdays.includes(time)) {
       workHours = await request('get', `${uri}/employee/${employee.id}` +
       `/workhours?weekday=${time}`);
       return message.reply(printHours(workHours));
+    }
+
+    try {
+      const date = humanDate(time);
+      if (date instanceof Date) {
+        const weekday = getWeekday(date);
+        workHours = await request('get', `${uri}/employee/${employee.id}` +
+        `/workhours?weekday=${weekday}`);
+        return message.reply(printHours(workHours));
+      }
+    } catch (e) {
+      bot.log.verbose(e);
     }
 
     workHours = await request('get', `${uri}/employee/${employee.id}/workhours`);
