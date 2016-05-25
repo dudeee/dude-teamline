80243e8abda45a8d371da31e8da31537979947a4
Mahdi Dibaiee <mdibaiee@aol.com>
2016-05-25 11:19:19 +0430

    fix(available): allow spaces between commas and names

--- src/commands/schedules/available.js
+++ src/commands/schedules/available.js
@@ -11,7 +11,7 @@ export default (bot, uri) => {
   moment.updateLocale('en', _.get(bot.config, 'moment') || {});
   moment.locale('en');
 
-  bot.command('^available [char] [string]', async message => {
+  bot.listen(/^available\s*((?:\([^)]+\))|(?:\w*))\s*(.*)/igm, async message => {
     const [username, vdate] = message.match;
     const employee = await findEmployee(uri, bot, message, username);
 
